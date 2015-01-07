bd6794e6b9cfa414f9324200eff402e59b356bc9
joominyeo <joominyeo@outlook.com>
2015-01-06 22:59:05 -0800

    dg
    sdfg

---
+++ ib-compsci-master/app/src/androidTest/java/com/example/cai/zetattendance/ApplicationTest.java
@@ -0,0 +1,17 @@
+package com.example.cai.zetattendance;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/*
+* Created and maintained by Joomin "Cai" Yeo @since Oct 2014
+*/
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
